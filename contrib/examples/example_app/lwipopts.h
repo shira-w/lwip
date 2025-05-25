ce8be3e999c1138eb9e0d16a51e4ec0380cb5cca
shiraw1 <SHIRAW1@rafael.co.il>
2025-05-25 22:54:54 +0300

    #define LWIP_SOCKET                0

--- contrib/examples/example_app/lwipopts.h
+++ contrib/examples/example_app/lwipopts.h
@@ -40,7 +40,7 @@
 #define LWIP_IPV6                  1
 
 #define NO_SYS                     0
-#define LWIP_SOCKET                (NO_SYS==0)
+#define LWIP_SOCKET                0
 #define LWIP_NETCONN               (NO_SYS==0)
 #define LWIP_NETIF_API             (NO_SYS==0)
 
