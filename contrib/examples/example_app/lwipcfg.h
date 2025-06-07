c6f6d3a1465c27459f088e8b729e00ad11bf9e31
shira-w <shirashirashira61@gmail.com>
2025-06-08 00:56:27 +0300

    receive 2

--- contrib/examples/example_app/lwipcfg.h
+++ contrib/examples/example_app/lwipcfg.h
@@ -26,7 +26,7 @@
 
 /* remember to change this MAC address to suit your needs!
    the last octet will be increased by netif->num for each netif */
-#define LWIP_MAC_ADDR_BASE            {0x00,0x01,0x02,0x03,0x04,0x05}
+#define LWIP_MAC_ADDR_BASE            {0x00,0x01,0x02,0x03,0x04,0x07}
 
 /* #define USE_SLIPIF 0 */
 /* #define SIO_USE_COMPORT 0 */
